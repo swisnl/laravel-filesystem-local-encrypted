f64d24ea4db97db301c76e0c863d5680869bb16c
Jasper Zonneveld <jasper@swis.nl>
2020-04-30 20:00:18 +0200

    Fix documentation issues found by Scrutinizer in response macro

--- src/EncryptedDataServiceProvider.php
+++ src/EncryptedDataServiceProvider.php
@@ -56,9 +56,9 @@ class EncryptedDataServiceProvider extends ServiceProvider
             'downloadEncrypted',
             /**
              * @param \SplFileInfo|string $file
-             * @param null $name
+             * @param string|null $name
              * @param array $headers
-             * @param string $disposition
+             * @param string|null $disposition
              *
              * @return \Swis\Laravel\Encrypted\BinaryFileResponse
              */
